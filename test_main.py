9afe0fbfbb6864f4dd8e639289c8983efc290cce
SonicX-svg <143742185+SonicX-svg@users.noreply.github.com>
2024-01-23 20:50:47 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,5 +5,6 @@ from steamlit.testing.v1 import AppTest
 def test_increment_and_add():
    at = AppTest.from_file('streamlit_app2.py').run()
    at = at.file_uploader[0].set_value('cat108.jpg')
+   assert response.status_code == 'cat'
 
    
