164c13b4adbffe442ea89156755b6090f8554f39
SonicX-svg <143742185+SonicX-svg@users.noreply.github.com>
2024-01-23 20:59:45 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,6 +5,8 @@ from steamlit.testing.v1 import AppTest
 def test_increment_and_add():
    at = AppTest.from_file('streamlit_app2.py').run()
    at = at.file_uploader[0].set_value('cat108.jpg')
-   assert response.status_code == 'cat'
+   at.button[0].click()    #симулируем нажатие кнопки распознавания
+             #получаем результат работы модели
+   assert 'cat' in response.status_code #сравниваем с действительный ответом
 
    
